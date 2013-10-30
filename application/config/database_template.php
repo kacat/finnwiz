0be90d9cbfc45f91ee2f6e530bf3c5319e1b95c4
Péter Frey <frey@audiodraft.com>
2013-10-30 15:22:50 +0200

    config updates
    added templates for config, database and htaccess
    
    Signed-off-by: Péter Frey <frey@audiodraft.com>

--- application/config/database_template.php
+++ application/config/database_template.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'webuser';
-$db['default']['password'] = 'webuser';
-$db['default']['database'] = 'grawiz';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
