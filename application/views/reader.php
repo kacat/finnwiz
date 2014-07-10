df643b591a5525cf0050de157c1c91aa6232ee51
Peter Frey <peter.frey@audiodraft.com>
2014-07-10 19:44:51 +0300

    added click functionality to bibook

--- application/views/reader.php
+++ application/views/reader.php
@@ -51,6 +51,33 @@
 		
 	});
 	
+	$('.lang_fi span').click(function(evt){
+		$('.lang_fi span').removeClass('selected');
+		$('.lang_en span').removeClass('selected');
+		
+		$(this).addClass('selected');
+		var myIndex = $('.lang_fi span').index(this);
+		var trg = $('.lang_en span:eq('+myIndex+')');
+		var mypos = $(this).position();
+		
+		//console.log(trg);
+		
+		if(trg.length){
+			var itpos = trg.position();
+		
+			//console.log(mypos.top, itpos.top);
+		
+			var diff = mypos.top - itpos.top;
+		
+			if ($('.lang_en .floater').css('top') != diff + 'px'){
+				$('.lang_en .floater').stop(true,true).animate({top: diff + 'px'}, 'fast');
+			}
+		
+			trg.addClass('selected');
+		}
+		
+	});
+	
 	$('.lang_en').height($('.lang_fi').height()); 
 	
 	$('.lang_en span').mouseover(function(evt){
@@ -76,5 +103,30 @@
 			trg.addClass('selected');
 		}
 		
-	})
+	});
+	
+	$('.lang_en span').click(function(evt){
+		$('.lang_fi span').removeClass('selected');
+		$('.lang_en span').removeClass('selected');
+		
+		$(this).addClass('selected');
+		var myIndex = $('.lang_en span').index(this);
+		var trg = $('.lang_fi span:eq('+myIndex+')');
+		var mypos = $(this).position();
+		
+		if(trg.length){
+			var itpos = trg.position();
+		
+			//console.log(mypos.top, itpos.top);
+		
+			var diff = itpos.top - mypos.top;
+		
+			if ($('.lang_en .floater').css('top') != diff + 'px'){
+				$('.lang_en .floater').stop(true,true).animate({top: diff + 'px'}, 'fast');
+			}
+		
+			trg.addClass('selected');
+		}
+		
+	});
 </script>
